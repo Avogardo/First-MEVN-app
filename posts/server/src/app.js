6a02f715589695c78ab23afbb04ce54eb7299d8d
Avogardo <jakub.i.wolny@gmail.com>
2018-11-26 19:58:10 +0100

    fetch posts from server and render them on view

--- posts/server/src/app.js
+++ posts/server/src/app.js
@@ -12,6 +12,9 @@ app.get('/posts', (req, res) => {
   res.send([{
     title: 'Hello World!',
     description: 'Hi there! How are you?',
+  }, {
+    title: 'Post 2!',
+    description: 'Hi there! Quite nice, and you?',
   }]);
 });
 
